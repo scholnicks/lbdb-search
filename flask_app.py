3a53c2fbf753afc5e173bcb386efd6a4b23fd1b1
Steve Scholnick <scholnicks@gmail.com>
2025-08-10 09:03:11 -0400

    Blackened

--- flask_app.py
+++ flask_app.py
@@ -8,21 +8,23 @@ import os
 app = Flask(__name__)
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-DATABASE = os.path.join(BASE_DIR, 'lbdb.db')
+DATABASE = os.path.join(BASE_DIR, "lbdb.db")
 
 SQL = """
     select book_title,auth_name  from v_search
     WHERE book_title LIKE ? OR auth_name LIKE ?
 """
 
+
 def query_books(keyword):
     conn = sqlite3.connect(DATABASE)
     cursor = conn.cursor()
-    cursor.execute(SQL,  (f'%{keyword}%', f'%{keyword}%'))
+    cursor.execute(SQL, (f"%{keyword}%", f"%{keyword}%"))
     results = cursor.fetchall()
     conn.close()
 
     return results
+
 
 @app.route("/", methods=["GET", "POST"])
 def search():
@@ -36,9 +38,10 @@ def search():
 
 @app.teardown_appcontext
 def close_connection(exception):
-    db = g.pop('db', None)
+    db = g.pop("db", None)
     if db is not None:
         db.close()
+
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8000)
